7902663045eea2a70f451a5f97a317ee677bc005
Sixty <hiliulin@aliyun.com>
2022-11-03 00:11:57 +0800

    feat: 增加loading和推荐也缓存

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -5,14 +5,22 @@ import * as actionTypes from './store/actionCreators';
 import RecommendList from '../../components/list/';
 import Scroll from '../../baseUI/scroll/index';
 import { Content } from './style';
+import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/loading/index';
 
 function Recommend(props){
-  const { bannerList, recommendList } = props;
+  const { bannerList, recommendList, enterLoading } = props;
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
   useEffect(() => {
-    getBannerDataDispatch();
-    getRecommendListDataDispatch();
+    // 如果页面有数据，则不发请求
+    //immutable 数据结构中长度属性 size
+    if (!bannerList.size){
+      getBannerDataDispatch ();
+    }
+    if (!recommendList.size){
+      getRecommendListDataDispatch ();
+    }
     //eslint-disable-next-line
   }, []);
 
@@ -21,12 +29,14 @@ function Recommend(props){
 
   return (
     <Content>
-      <Scroll>
+      <Scroll className="list" onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
+
+      { enterLoading ? <Loading></Loading> : null }
     </Content> 
   );
 }
@@ -37,6 +47,7 @@ const mapStateToProps = (state) => ({
   // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
   bannerList: state.getIn(['recommend', 'bannerList']),
   recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn (['recommend', 'enterLoading'])
 });
 // 映射 dispatch 到 props 上
 const mapDispatchToProps = (dispatch) => {
--- src/application/Recommend/store/reducer.js
+++ src/application/Recommend/store/reducer.js
@@ -4,6 +4,7 @@ import { fromJS } from 'immutable';// 这里用到 fromJS 把 JS 数据结构转
 const defaultState = fromJS ({
   bannerList: [],
   recommendList: [],
+  enterLoading: true,
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -13,6 +14,8 @@ export default (state = defaultState, action) => {
       return state.set('bannerList', action.data);
     case actionTypes.CHANGE_RECOMMEND_LIST:
       return state.set('recommendList', action.data);
+    case actionTypes.CHANGE_ENTER_LOADING:
+      return state.set('enterLoading', action.data);
     default:
       return state;
   }
