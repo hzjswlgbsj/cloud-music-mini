ba7ac5a873884e0b0e6dc752ca8a9aa746845ff4
Sixty <hiliulin@aliyun.com>
2022-11-04 22:23:16 +0800

    feat: 增加歌手页

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,24 +1,136 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Horizen from '../../baseUI/horizen-item';
+import LazyLoad, { forceCheck } from 'react-lazyload';
 import { categoryTypes, alphaTypes } from '../../api/config';
-import { NavContainer } from './style';
+import { NavContainer, ListContainer, List, ListItem } from './style';
+import Scroll from '../../baseUI/scroll'
+import { 
+  getSingerList, 
+  getHotSingerList, 
+  changeEnterLoading, 
+  changePageCount, 
+  refreshMoreSingerList, 
+  changePullUpLoading, 
+  changePullDownLoading, 
+  refreshMoreHotSingerList 
+} from './store/actionCreators';
+import {connect} from 'react-redux';
+import Loading from '../../baseUI/loading';
 
 function Singers(props) {
-  let [category, setCategory] = useState('')
-  let [alpha, setAlpha] = useState('')
+  let [category, setCategory] = useState('');
+  let [alpha, setAlpha] = useState('');
+
+  const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount } = props;
+
+  const { getHotSingerDispatch, updateDispatch, pullDownRefreshDispatch, pullUpRefreshDispatch } = props;
+
+  useEffect(() => {
+    getHotSingerDispatch();
+    // eslint-disable-next-line
+  }, []);
+
   let handleUpdateAlpha = (val) => {
-    setAlpha(val)
-  }
+    setAlpha(val);
+    updateDispatch(category, val);
+  };
+
   let handleUpdateCategory = (val) => {
-    setCategory(val)
-  }
+    setCategory(val);
+    updateDispatch(val, alpha);
+  };
+
+  const handlePullUp = () => {
+    pullUpRefreshDispatch(category, alpha, category === '', pageCount);
+  };
+
+  const handlePullDown = () => {
+    pullDownRefreshDispatch(category, alpha);
+  };
+
+  const renderSingerList = () => {
+    const list = singerList ? singerList.toJS(): [];
+    return (
+      <List>
+        {
+          list.map((item, index) => {
+            return (
+              <ListItem key={item.accountId+""+index}>
+                <div className="img_wrapper">
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./singer.png')} alt="music"/>}>
+                    <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                  </LazyLoad>
+                </div>
+                <span className="name">{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+      </List>
+    )
+  };
 
   return (
-    <NavContainer>
-      <Horizen list={categoryTypes} title={"分类(默认热门):"} handleClick={handleUpdateCategory} oldVal={category}></Horizen>
-      <Horizen list={alphaTypes} title={"首字母:"} handleClick={(handleUpdateAlpha)} oldVal={alpha}></Horizen>
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horizen list={categoryTypes} title={"分类(默认热门):"} handleClick={handleUpdateCategory} oldVal={category}></Horizen>
+        <Horizen list={alphaTypes} title={"首字母:"} handleClick={(handleUpdateAlpha)} oldVal={alpha}></Horizen>
+      </NavContainer> 
+      <ListContainer>
+        <Scroll
+          pullUp={ handlePullUp }
+          pullDown = { handlePullDown }
+          pullUpLoading = { pullUpLoading }
+          pullDownLoading = { pullDownLoading }
+          onScroll={forceCheck}
+        >
+          { renderSingerList() }
+        </Scroll>
+        <Loading show={enterLoading}></Loading>
+      </ListContainer>
+    </div>
   )
 }
 
-export default React.memo(Singers)
+const mapStateToProps = (state) => ({
+  singerList: state.getIn(['singers', 'singerList']),
+  enterLoading: state.getIn(['singers', 'enterLoading']),
+  pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+  pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+  pageCount: state.getIn(['singers', 'pageCount'])
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getHotSingerDispatch() {
+      dispatch(getHotSingerList());
+    },
+    updateDispatch(category, alpha) {
+      dispatch(changePageCount(0));//由于改变了分类，所以pageCount清零
+      dispatch(changeEnterLoading(true));//loading，现在实现控制逻辑，效果实现放到下一节，后面的loading同理
+      dispatch(getSingerList(category, alpha));
+    },
+    // 滑到最底部刷新部分的处理
+    pullUpRefreshDispatch(category, alpha, hot, count) {
+      dispatch(changePullUpLoading(true));
+      dispatch(changePageCount(count+1));
+      if(hot){
+        dispatch(refreshMoreHotSingerList());
+      } else {
+        dispatch(refreshMoreSingerList(category, alpha));
+      }
+    },
+    //顶部下拉刷新
+    pullDownRefreshDispatch(category, alpha) {
+      dispatch(changePullDownLoading(true));
+      dispatch(changePageCount(0));//属于重新获取数据
+      if(category === '' && alpha === ''){
+        dispatch(getHotSingerList());
+      } else {
+        dispatch(getSingerList(category, alpha));
+      }
+    }
+  }
+};   
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singers));
--- src/baseUI/loading/index.js
+++ src/baseUI/loading/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import style from '../../assets/global-style';
+import { PropTypes } from 'prop-types';
 
 const loading = keyframes`
   0%, 100% {
@@ -11,29 +12,41 @@ const loading = keyframes`
   }
 `
 const LoadingWrapper = styled.div`
-    >div {
-      position: absolute;
-      top: 0; left: 0; right: 0; bottom: 0;
-      margin: auto;
-      width: 60px;
-      height: 60px;
-      opacity: 0.6;
-      border-radius: 50%;
-      background-color: ${style["theme-color"]};
-      animation: ${loading} 1.4s infinite ease-in;
-    }
-    >div:nth-child(2) {
-      animation-delay: -0.7s;
-    }
+  >div {
+    position: absolute;
+    left: 0; 
+    right: 0;  
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 60px;
+    height: 60px;
+    opacity: 0.6;
+    border-radius: 50%;
+    background-color: ${style["theme-color"]};
+    animation: ${loading} 1.4s infinite ease-in;
+  }
+  >div:nth-child(2) {
+    animation-delay: -0.7s;
+  }
 `
 
-function Loading()  {
+function Loading(props) {
+  const { show } = props;
   return (
-    <LoadingWrapper>
+    <LoadingWrapper style={show ? {display: ""}: {display: "none"}}>
       <div></div>
       <div></div>
     </LoadingWrapper>
   );
 }
+
+Loading.defaultProps = {
+  show: true
+};
+
+Loading.propTypes = {
+  show: PropTypes.bool
+};
  
 export default React.memo(Loading);
